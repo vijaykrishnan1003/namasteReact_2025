aa132774c1f6e996cc4df8513d2b915fa7bc7ad5
vijaykrishnan1003 <vijaykrishnan1003@gmail.com>
2025-01-04 11:00:53 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
         const parent = React.createElement("div", {id: "parent"},[
             React.createElement("div",{id :"child1"},
