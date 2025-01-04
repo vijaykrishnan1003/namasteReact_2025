a59ce87ead13244b0975534e4f5f6dcbde196cb6
vijaykrishnan1003 <vijaykrishnan1003@gmail.com>
2025-01-04 17:11:27 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-        const parent = React.createElement("div", {id: "parent"},[
-            React.createElement("div",{id :"child1"},
-                [React.createElement("h1",{},"im h1 tag"), React.createElement("h2",{},"Im h2 tag")]
-            ),
-            React.createElement("div",{id :"child2"},
-                [React.createElement("h1",{},"im h1 tag"), React.createElement("h2",{},"Im h2 tag")]
-            ),
-        ]);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+const jsxHeading = <h1 id= "heading">Namaste React from JSX u</h1>;
 
-        root.render(parent);
+const Title = ()=> {
+    return <h2 id= "title">Namaste React from Title 😡</h2>;
+}
+
+const BodyWord = ()=> {
+    return <p id= "body">Namaste React from Body 😡</p>;
+}
+
+const num = 9999;
+const HeadingComponent = ()=> (
+    <div id="container">
+        <Title/>
+        {jsxHeading}
+        <h1>Hello Functional component from React yyy</h1>
+        <BodyWord/>
+    </div>
+     
+    );
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
